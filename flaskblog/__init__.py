9038d81c24100aa3bd34380130b582e983057fbd
Patryk <parol.patryk@gmail.com>
2023-04-05 00:11:21 +0200

    auth users validate username and email, logout functionality and small changes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = 'b68dc391d06a7a6812c4932d567854c2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
